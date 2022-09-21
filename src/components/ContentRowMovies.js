c93cb848dc4049b3a9c5e10c8d8d5a47b1da70a8
Rodrione1 <alvarezrodrigofernando@gmail.com>
2022-09-21 20:54:26 -0300

    clase 44

---
+++ src/components/ContentRowMovies.js
@@ -0,0 +1,19 @@
+import React from "react";
+import  ContentRowMovies  from "./ContentRowMovies";
+import TopBar from './TopBar';
+import Footer from './Footer';
+
+
+// function ContentRowMovies(){
+//   return (
+//     <div id="content-wrapper" className="d-flex flex-column">
+//     <div id="content">
+//       <TopBar />
+//        <ContentRowMovies /> 
+//       <Footer />
+//     </div>
+//     </div>
+//   )
+// }
+
+export default ContentRowMovies;
